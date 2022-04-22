4749b18e171226ab4746c443c84eaf751a93acd1
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-04-22 11:34:32 +0200

    first commit11

---
+++ newapp/src/Header.js
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom'
+
+function Header () {
+  return (
+    <header className ="header">
+     <div className="header-logo"
+       <img src ={logo} alt =""/>
+       </div>
+       <Menu>
+       </Menu>
+    </header>
+    )
+}
